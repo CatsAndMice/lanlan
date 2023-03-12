db29f05cfbb96aef1ea2348cd26e8ffa5e52d9e0
lihai <2322873900@qq.com>
2023-03-12 09:58:04 +0800

    :art:Github链接修改

--- App.js
+++ App.js
@@ -58,7 +58,7 @@ const App = () => {
             <div className="flex flex-wrap justify-center space-x-5">
                 <a
                     className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-4 py-2 text-sm text-gray-600 shadow-md transition-colors hover:bg-gray-100 mb-5"
-                    href="https://github.com/shengxinjing/benben"
+                    href="https://github.com/CatsAndMice/lanlan"
                     target="_blank"
                     rel="noopener noreferrer"
                 >
